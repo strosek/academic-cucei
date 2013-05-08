7a954765ed9cdd161a21738f63de11f6c962dd80
Erick Daniel Corona <edcoronag@gmail.com>
2013-05-07 20:25:22 -0500

    added some code to decrypt program

--- cryptography/decrypt.c
+++ cryptography/decrypt.c
@@ -3,9 +3,22 @@
 
 #include <omp.h>
 
+const int FILE_NAME_LENGTH = 100 + 1;
+
 
 int main(void) {
-  
+  char fileName[FILE_NAME_LENGTH];
+  printf("DECRYPT PROGRAM\n");
+  printf("===============\n\n");
+
+  printf("Running shared-memory parallellism using [%d] threads...", 
+         omp_get_max_threads());
+
+  printf("Name of file to decrypt: ");
+  scanf("%s", fileName);
+
+  FILE* file = fopen(fileName, "wb");
 
   return EXIT_SUCCESS;
 }
+
