95e78b2620f7a357c4f8ea5f06e66edd80458615
Erick Daniel Corona <edcoronag@gmail.com>
2014-05-17 17:26:47 -0500

    implemented cannon's algorithm for omp

--- parallel/matmul_omp.c
+++ parallel/matmul_omp.c
@@ -1,7 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <omp.h>
+#define DEBUG 1
+
+
+int ** allocateMatrix(size_t m, size_t n);
+void freeMatrix(int ** const matrix, size_t n);
+void initialize(int ** const matrix, size_t m, size_t n);
+void multiplyMatrices(const int ** const a, const int ** const b,
+                      int ** const ab, size_t size);
+void printMatrix(const int ** const matrix, size_t m, size_t n);
+void shiftColumn(int ** const matrix, size_t n, size_t column, size_t times);
+void shiftRow(int ** const matrix, size_t m, size_t row, size_t times);
+
+
+int main(void)
+{
+  const size_t SIZE_M = 4UL;
+  const size_t SIZE_N = SIZE_M;
+  const size_t SIZE_P = SIZE_M;
+
+  int ** matA = allocateMatrix(SIZE_M, SIZE_N);
+  int ** matB = allocateMatrix(SIZE_P, SIZE_M);
+  int ** matC = allocateMatrix(SIZE_P, SIZE_N);
+  
+  initialize(matA, SIZE_M, SIZE_N);
+  initialize(matB, SIZE_M, SIZE_N);
+  
+#ifdef DEBUG
+  printf("A: \n");
+  printMatrix((const int ** const)matA, SIZE_M, SIZE_N);
+  printf("B: \n");
+  printMatrix((const int ** const)matB, SIZE_M, SIZE_N);
+  
+  printf("Shifting...\n");
+#endif
+
+#pragma omp parallel for
+  for (size_t i = 1UL; i < SIZE_N; ++i)
+  {
+    shiftRow(matA, SIZE_M, i, i);
+  }
+#pragma omp parallel for
+  for (size_t i = 1UL; i < SIZE_M; ++i)
+  {
+    shiftColumn(matB, SIZE_N, i, i);
+  }
+
+#ifdef DEBUG
+  printf("A shifted: \n");
+  printMatrix((const int ** const)matA, SIZE_M, SIZE_N);
+  printf("B shifted: \n");
+  printMatrix((const int ** const)matB, SIZE_M, SIZE_N);
+#endif
+
+  for (size_t i = 0UL; i < SIZE_N; ++i)
+  {
+    multiplyMatrices((const int ** const)matA, (const int ** const)matB, matC,
+                     SIZE_M);
+    shiftRow(matA, SIZE_M, 0, 1);
+    shiftColumn(matB, SIZE_P, 0, 1);
+  }
+
+#ifdef DEBUG
+  printf("AB: \n");
+  printMatrix((const int ** const)matC, SIZE_M, SIZE_N);
+#endif
+
+  freeMatrix(matC, SIZE_N);
+  freeMatrix(matB, SIZE_N);
+  freeMatrix(matA, SIZE_N);
+
+  return EXIT_SUCCESS;
+}
 
 
 void shiftRow(int ** const matrix, size_t m, size_t row, size_t times)
@@ -34,11 +105,16 @@ void shiftColumn(int ** const matrix, size_t n, size_t column, size_t times)
   }
 }
 
-void multiply(const int ** const a, const int ** const b, int ** const ab,
-              size_t m, size_t n, size_t p)
+void multiplyMatrices(const int ** const a, const int ** const b,
+                      int ** const ab, size_t size)
 {
-  for (size_t i = 0UL; i < m; ++i)
+#pragma omp parallel for
+  for (size_t i = 0UL; i < size; ++i)
   {
+    for (size_t j = 0UL; j < size; ++j)
+    {
+      ab[i][j] = a[i][j] * b[i][j];
+    }
   }
 }
 
@@ -56,7 +132,7 @@ void printMatrix(const int ** const matrix, size_t m, size_t n)
 
 void initialize(int ** const matrix, size_t m, size_t n)
 {
-#pragma omp for
+#pragma omp parallel for
   for (size_t i = 0UL; i < n; ++i)
   {
     for (size_t j = 0UL; j < m; ++j)
@@ -79,52 +155,11 @@ int ** allocateMatrix(size_t m, size_t n)
 
 void freeMatrix(int ** const matrix, size_t n)
 {
-#pragma omp for
+#pragma omp parallel for
   for (size_t i = 0UL; i < n; ++i)
   {
     free((int**)matrix[i]);
   }
   free((int*)matrix);
-}
-
-int main(void)
-{
-  const size_t SIZE_M = 10UL;
-  const size_t SIZE_N = SIZE_M;
-  const size_t SIZE_P = SIZE_M;
-
-  int ** matA = allocateMatrix(SIZE_M, SIZE_N);
-  int ** matB = allocateMatrix(SIZE_P, SIZE_M);
-  int ** matC = allocateMatrix(SIZE_P, SIZE_N);
-  
-  initialize(matA, SIZE_M, SIZE_N);
-  initialize(matB, SIZE_M, SIZE_N);
-  
-  printf("A: \n");
-  printMatrix((const int ** const)matA, SIZE_M, SIZE_N);
-  printf("B: \n");
-  printMatrix((const int ** const)matB, SIZE_M, SIZE_N);
-  
-  printf("Shifting...\n");
-#pragma omp for
-  for (size_t i = 0UL; i < SIZE_N; ++i)
-  {
-    shiftRow(matA, SIZE_M, i, i);
-  }
-#pragma omp for
-  for (size_t i = 0UL; i < SIZE_M; ++i)
-  {
-    shiftColumn(matB, SIZE_N, i, i);
-  }
-  
-  printf("A: \n");
-  printMatrix((const int ** const)matA, SIZE_M, SIZE_N);
-  printf("B: \n");
-  printMatrix((const int ** const)matB, SIZE_M, SIZE_N);
-
-  freeMatrix(matB, SIZE_N);
-  freeMatrix(matA, SIZE_N);
-  
-  return EXIT_SUCCESS;
 }
 
