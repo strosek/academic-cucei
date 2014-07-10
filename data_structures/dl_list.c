0622cd7f1b431dd9eae054bca34e294c8d8eace5
Erick Daniel Corona <edcoronag@gmail.com>
2014-07-10 16:33:00 -0500

    minor fixes, completed modifiers code

--- data_structures/dl_list.c
+++ data_structures/dl_list.c
@@ -6,11 +6,19 @@
 
 
 Data_t front(Node_t * list) {
-  return list->data;
+  if (list != NULL)
+  {
+    return list->data;
+  }
+  return ELEMENT_INVALID;
 }
 
 Data_t back(Node_t * list) {
-  return list->prev->data;
+  if (list != NULL)
+  {
+    return list->prev->data;
+  }
+  return ELEMENT_INVALID;
 }
 
 Node_t * at(Node_t * list, size_t position) {
@@ -41,12 +49,27 @@ int isEmpty(Node_t * list) {
   return list == NULL;
 }
 
-void print(Node_t * list) {
+void printList(Node_t * list) {
   if (list != NULL) {
     Node_t * temp = list;
     size_t nodeNo = 0;
     while (temp->next != list && nodeNo != 0) {
-      printf("%lu: %d", nodeNo, temp->data);
+      printf("%4lu: %d", nodeNo, temp->data);
+
+      temp = temp->next;
+      ++nodeNo;
+    }
+  }
+}
+
+void printReversedList(Node_t * list) {
+  if (list != NULL) {
+    Node_t * temp = list;
+    size_t nodeNo = 0;
+    while (temp->prev != list && nodeNo != 0) {
+      printf("%4lu: %d", nodeNo, temp->data);
+
+      temp = temp->prev;
       ++nodeNo;
     }
   }
@@ -68,8 +91,9 @@ Node_t * clear(Node_t * list) {
       tempNext = tempNext->next;
     }
   }
+  list = NULL;
 
-  return NULL;
+  return list;
 }
 
 Node_t * pushFront(Node_t * list, Data_t element) {
@@ -88,69 +112,167 @@ Node_t * pushFront(Node_t * list, Data_t element) {
     newNode->prev->next = newNode;
   }
 
-  return newNode;
+  return list;
 }
 
 Node_t * pushBack(Node_t * list, Data_t element) {
-  Node_t * newNode = (Node_t *)malloc(sizeof(Node_t));
-  newNode->data = element;
+  if (list != NULL)
+  {
+    Node_t * newNode = (Node_t *)malloc(sizeof(Node_t));
+    newNode->data = element;
 
-  if (isEmpty(list)) {
-    newNode->next = newNode;
-    newNode->prev = newNode;
-    return newNode;
-  }
-  else {
-    list->prev->next = newNode;
-    newNode->next = list;
-    newNode->prev = list->prev;
-    list->prev = newNode;
+    if (isEmpty(list)) {
+      newNode->next = newNode;
+      newNode->prev = newNode;
+      return newNode;
+    }
+    else {
+      list->prev->next = newNode;
+      newNode->next = list;
+      newNode->prev = list->prev;
+      list->prev = newNode;
+    }
   }
 
   return list;
 }
 
 Node_t * popFront(Node_t * list) {
-  Node_t * temp = list->next;
-  list->next->prev = list->prev;
-  list->prev->next = list->next;
-  free(list);
-  return temp;
+  if (list != NULL)
+  {
+    list->next->prev = list->prev;
+    list->prev->next = list->next;
+    free(list);
+  }
+
+  return list;
 }
 
 Node_t * popBack(Node_t * list) {
-  Node_t * temp = list->prev;
-  list->prev->prev->next = list;
-  list->prev = list->prev->prev;
-  free(temp);
+  if (list != NULL)
+  {
+    Node_t * temp = list->prev;
+    list->prev->prev->next = list;
+    list->prev = list->prev->prev;
+    free(temp);
+  }
+
   return list;
 }
 
 Node_t * insert(Node_t * list, Node_t * position, Data_t element) {
   Node_t * temp = list;
+  Node_t * newNode = (Node_t *)malloc(sizeof(Node_t));
+  newNode->data = element;
+
+  if (list != NULL && position != NULL)
+  {
+    return NULL;
+  }
 
   if (position == list) {
     pushFront(list, element);
   }
   while (temp != position && temp != list) {
     temp = temp->next;
+
+    if (temp == position)
+    {
+      newNode->prev = position;
+      newNode->next = position->next;
+      position->next->prev = newNode;
+      position->next = newNode;
+    }
   }
+
   return list;
 }
 
 Node_t * insertAt(Node_t * list, size_t position, Data_t element) {
+  if (list != NULL)
+  {
+    Node_t * temp = list;
+    size_t i = 0;
+    while (i <= position && temp->next != NULL)
+    {
+      if (i == position)
+      {
+        Node_t * newNode = (Node_t *)malloc(sizeof(Node_t));
+        newNode->data = element;
+
+        newNode->next = temp;
+        newNode->prev = temp->prev;
+        temp->prev->next = newNode;
+        temp->prev = newNode;
+      }
+
+      temp = temp->next;
+      ++i;
+    }
+  }
+
   return list;
 }
 
 Node_t * erase(Node_t * list, Node_t * position) {
+  if (list != NULL)
+  {
+    Node_t * temp = list;
+    while (temp != position && temp->next != NULL)
+    {
+      if (temp == position)
+      {
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        free(temp);
+      }
+
+      temp = temp->next;
+    }
+  }
+
   return list;
 }
 
 Node_t * eraseAt(Node_t * list, size_t position) {
+  if (list != NULL)
+  {
+    Node_t * temp = list;
+    size_t i = 0;
+    while (i <= position && temp->next != NULL)
+    {
+      if (i == position)
+      {
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        free(temp);
+      }
+
+      temp = temp->next;
+      ++i;
+    }
+  }
+
   return list;
 }
 
-Node_t * removeNode(Node_t * list, Data_t key) {
+Node_t * removeKey(Node_t * list, Data_t key) {
+  if (list != NULL)
+  {
+    Node_t * temp = list;
+    while (temp->data != key && temp->next != NULL)
+    {
+      if (temp->data == key)
+      {
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        free(temp);
+      }
+
+      temp = temp->next;
+    }
+  }
+
   return list;
 }
 
--- data_structures/dl_list.h
+++ data_structures/dl_list.h
@@ -1,9 +1,11 @@
 /*  Doubly linked list */
 
 #include <stdlib.h>
+#include <limits.h>
 
 
 typedef int Data_t;
+const int ELEMENT_INVALID = INT_MIN;
 
 typedef struct n {
   Data_t data;
@@ -18,7 +20,8 @@ Data_t   back(Node_t * list);
 Node_t * at(Node_t * list, size_t position);
 Node_t * search(Node_t * list, Data_t element);
 int      isEmpty(Node_t * list);
-void     print();
+void     printList();
+void     printReversedList();
 
 
 /* Modifiers */
@@ -32,6 +35,6 @@ Node_t * insert(Node_t * list, Node_t* position, Data_t element);
 Node_t * insertAt(Node_t * list, size_t position, Data_t element);
 Node_t * erase(Node_t * list, Node_t* position);
 Node_t * eraseAt(Node_t * list, size_t position);
-Node_t * removeNode(Node_t * list, Data_t key);
+Node_t * removeKey(Node_t * list, Data_t key);
 Node_t * sort(Node_t * list);
 
